533985583f55e657ee77ef4369e14b4a93968295
Kr1ner <47250537+Kr1ner@users.noreply.github.com>
2022-01-09 01:12:57 +0400

    update

--- 4C.cpp
+++ 4C.cpp
@@ -1,2 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n;
+map <string,int> usr;
+int main(){
+    cin>>n;
+    for (int i=0 ; i<n ;i++){
+        string x;
+        cin>>x;
+        if(usr[x]==0){
+            cout<<"OK"<<endl;
+            usr[x]=1;
+        }
+        else{
+            cout<<x<<usr[x]<<endl;
+            usr[x]++;
+        }
+    }
+}
