8f3e6af54f1ebd0e45ed0d7ac87e547d165e9059
Kr1ner <47250537+Kr1ner@users.noreply.github.com>
2022-01-08 21:19:45 +0400

    Update 230A.cpp

--- 230A.cpp
+++ 230A.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 int s,n;
 int main(){
-    
+    cin>>s>>n;
+    int x[n],y[n];
+    for (int i = 0; i < n; i++){
+        cin>>x[i]>>y[i];
+    }
+    sort(x,x+n);
+    sort(y,y+n);
+    for (int i = 0; i < n; i++)
+    {
+       if(s<=x[i]){
+           cout<<"NO";
+           return 0;
+       }
+       else{
+           x+=y[i];
+       }
+    }
+    cout<<"YES";
 }
