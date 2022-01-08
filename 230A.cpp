685d43433b03df028cf91090cc71133ba5b2e60d
Kr1ner <47250537+Kr1ner@users.noreply.github.com>
2022-01-08 21:00:33 +0400

    Update 230A.cpp

--- 230A.cpp
+++ 230A.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 int s,n;
 int main(){
-    
+    cin>>s>>n;
+    while(n--){
+        int x,y;
+        cin>>x>>y;
+        if(s<=x){
+            cout<<"NO";
+            return 0;
+        }
+        else{
+            s+=y;
+        }
+    }
+    cout<<"YES";
 }
