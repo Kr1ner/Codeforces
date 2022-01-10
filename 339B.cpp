5dcb017c3f5874daa9f5db10e7102eb66c9b3d99
Kriner <krineri12@gmail.com>
2022-01-10 16:17:52 +0400

    new problem

--- 339B.cpp
+++ 339B.cpp
@@ -1,3 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-int 
+long long n,m,counter;
+int main ()
+{
+	cin>>n>>m;
+    int a[m];
+	for (int i=1; i<=m; i++){
+	    cin>>a[i];
+    }
+    int a1=1;
+    for (int i=1; i<=m; i++){
+        if (a[i]>a1) { 
+            counter+=a[i]-a1;
+            a1=a[i];
+        }
+        else if (a[i]<a1) {
+            counter+=n-a1+a[i];
+            a1=a[i];
+        }
+    }
+    cout<<counter<<endl;
+}
