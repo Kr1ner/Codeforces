e325dfe2063d3c5f8fb4bdb04475425f3368d38f
Irakli <krineri12@gmail.com>
2022-01-12 17:26:00 +0400

    gigi

--- 427A.cpp
+++ 427A.cpp
@@ -3,4 +3,28 @@ using namespace std;
 int n;
 int main(){
     cin>>n;
+    int x[n],i=1;
+    cin>>x[0];
+    int crm = 0,rec = 0;
+    if(x[0]==-1){
+        crm=1;
+    }
+    else{
+        rec++;
+    }
+    while(n--){
+        cin>>x[i];
+        if(x[i]==-1){
+            if(rec>0){
+                rec--;
+            }
+            else{
+                crm++;
+            }
+        }
+        else{
+            rec++;
+        }
+    }
+    cout<<crm;
 }
