f2a1cff35601adf31eab48b503a87e956295457f
Kr1ner <krineri12@gmail.com>
2022-01-18 13:17:07 +0400

    despacito

--- 432A.cpp
+++ 432A.cpp
@@ -1,1 +1,16 @@
-sda
+#include <bits/stdc++.h>
+using namespace std;
+int n,k;
+int main(){
+    cin>>n>>k;
+    int a[n],cnt;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        if(5-a[i]>=k){
+            cnt++;
+        }
+    }
+    cout<<cnt/3;
+}
