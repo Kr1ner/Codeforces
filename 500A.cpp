ec455a9e6b825cd8635c3e2dbb79329c4e9d9cc9
Kr1ner <krineri12@gmail.com>
2022-01-23 19:12:36 +0400

    Update 500A.cpp

--- 500A.cpp
+++ 500A.cpp
@@ -1,24 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n,t,ans;
+int n,t;
 int main(){
     cin>>n>>t;
-    int a[n],t1=t-1;
-    for(int i=0;i<n-1;i++){
-        cin>>a[i];
+    int a[n-1];
+    for(int i=0; i<(n-1); i++)cin>>a[i];
+    for(int i=0; i<(n-1); ){
+        i=i+a[i];
+        if ( i==(t-1) ) { cout<<"YES"<<endl; return 0;}
     }
-    int i=a[0]+1;
-    while(t1--){
-        i+=a[i];
-        if(i==t){
-            cout<<"YES";
-            return 0;
-        }
-        else{
-            ans=1;
-        }
-    }
-    if(ans==1){
-        cout<<"NO";
-    }
+    cout<<"NO"<<endl;
+    return 0;
 }
