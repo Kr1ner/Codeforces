57bafbad4647820cd2050f6fef3a0ccb6b5da058
Irakli <47250537+Kr1ner@users.noreply.github.com>
2023-04-24 20:43:34 +0400

    upd

--- 1822A.cpp
+++ 1822A.cpp
@@ -1,1 +1,18 @@
 #include <iostream>
+using namespace std;
+int q;
+
+int main(){
+    cin>>q;
+    while(q--){
+        int n,t;
+        cin>>n>>t;
+        int arr[n-1][n-1][n-1];
+        int i=1;
+        while(i<n-1){
+            int a,b;
+            cin>>a>>b;
+            arr[i][i][i]=0;
+        }
+    }    
+}
