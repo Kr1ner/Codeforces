2e34997334ddc6ac64e70dddb8685c45e1e30d69
Irakli <47250537+Kr1ner@users.noreply.github.com>
2023-06-09 12:07:10 +0400

    upd

--- 1840B.cpp
+++ 1840B.cpp
@@ -4,13 +4,7 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        long long int n,k,i=0,cnt=1;
+        long long int n,k,intlim,sumlim,sumlim1;
         cin>>n>>k;
-        while(n-pow(2,i)>=0 && i<k){
-            n=n-pow(2,i);
-            i++;
-            cnt+=i;
-        }
-        cout<<cnt<<endl;
     }
 }
