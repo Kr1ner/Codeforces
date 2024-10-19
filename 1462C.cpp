724dd6ceadb75072e4dc1035f7967b750f5098f8
Irakli <krineri12@gmail.com>
2024-10-19 12:23:23 +0400

    upd

---
+++ 1462C.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        for(int i=9;i>1;i--){
+            while(x-9>x){
+                
+            }
+        }
+    }
+}
