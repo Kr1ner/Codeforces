5f57a15d923f30c55c45402a87ef5c48853edd26
kr1ner <krineri12@gmail.com>
2025-09-06 15:58:52 +0400

    update

--- 2132C1.cpp
+++ 2132C1.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main(){
   int t;
   cin>>t;
-  while(t--){
+  while(t){
     t--;
     long long n;
     cin>>n;
     long long total_cost = 0;
-    int powerOfThree = 1,x=0;
+    long long powerOfThree = 1,x=0;
     while(n>0){
       long long digit = n % 3;
       long long cpd = 3 * powerOfThree + x * (powerOfThree/3);
